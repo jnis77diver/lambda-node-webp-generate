1e2195ff908e738a5a25224410b06f1b4c8a8e39
Jonah Nisenson <jsnisenson@gmail.com>
2021-09-13 20:30:04 -0500

    change prefix to ignore to media/raw_images

--- common/common.js
+++ common/common.js
@@ -3,7 +3,7 @@ const OPTIONS = {
   QUALITY: 75, // image quality for WebP conversion
   IMG_EXTENSIONS: new Set([".jpg", ".jpeg", ".png"]),
   WEBP_EXT: ".webp",
-  PREFIXES_TO_IGNORE: new RegExp("^(static/raw_images|webp/static/raw_images)", "i"), // to add to this, just add more | with parens
+  PREFIXES_TO_IGNORE: new RegExp("^(media/raw_images|webp/media/raw_images)", "i"), // to add to this, just add more | with parens
 };
 
 module.exports = {
